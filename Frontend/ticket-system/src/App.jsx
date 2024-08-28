cb9af97d87b1a0eac3c6661627fba74f45141786
Omar Hemedi Jumaa <omarhemed94@gmail.com>
2024-08-28 16:46:46 +0300

    chore(frontend, backend): setup initial project structure
    - Initialized frontend with React.
    - Set up backend with Django.
    - Created basic project files and configuration for both frontend and backend.

---
+++ Frontend/ticket-system/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function App () {
+  return (
+    <div className="App">
+      <header className="text-red-800">
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  );
+}
