fd302301ba3ca116eacd1c2c01d5c90b447fa1e0
Omar Hemedi Jumaa <omarhemed94@gmail.com>
2024-09-04 19:21:45 +0300

    feat: Update Router configuration
    - Added AuthenticatedLayout to manage authenticated user routes
    - Added UserDashboardPage under /user/dashboard route

--- Frontend/ticket-system/src/routes/Router.jsx
+++ Frontend/ticket-system/src/routes/Router.jsx
@@ -5,6 +5,8 @@ import GuestLayout from "../layout/GuestLayout";
 import NotFoundPage from "../pages/errors/NotFoundPage";
 import ResetPasswordPage from "../pages/auth/ResetPasswordPage";
 import OtpPage from "../pages/auth/OtpPage";
+import AuthenticatedLayout from "../layout/AuthenticatedLayout";
+import UserDashboardPage from "../pages/user/UserDashboardPage";
 
 const Router = createBrowserRouter([
     {
@@ -31,6 +33,13 @@ const Router = createBrowserRouter([
         element: <GuestLayout />,
         children: [
             { path: "", element: <ResetPasswordPage /> },
+        ],
+    },
+    {
+        path: "/user",
+        element: <AuthenticatedLayout />,
+        children: [
+            { path: "dashboard", element: <UserDashboardPage /> },
         ],
     },
 ]);
