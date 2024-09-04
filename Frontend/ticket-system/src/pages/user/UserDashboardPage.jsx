42373a717d6bfc83de657efaae6d619f2c62b40b
Omar Hemedi Jumaa <omarhemed94@gmail.com>
2024-09-04 19:16:24 +0300

    feat: Add UserDashboardPage component
    - Created UserDashboardPage component for user dashboard view

---
+++ Frontend/ticket-system/src/pages/user/UserDashboardPage.jsx
@@ -0,0 +1,7 @@
+export default function UserDashboardPage()  {
+    return (
+        <h1 className="text-indigo-900 text-center">
+            This is the user dashboard page
+        </h1>
+    );
+}
